0e3e5101d19da8f3482f3176ca054b8d7f72aa72
Peter Coles <peter@mrcoles.com>
2018-01-25 19:33:48 -0500

    add slides

--- web/src/Footer.jsx
+++ web/src/Footer.jsx
@@ -7,6 +7,8 @@ const footer = props => {
   return (
     <footer>
       <p className="text-center">
+        View slides: <a href="DateKnight.pdf">pdf</a>
+        <br />
         Made with ♥ in NYC <br />
         &copy; {this_year}
       </p>
