89344a12b6753d3be8e740415173867b5010d31b
Peter Coles <peter@mrcoles.com>
2018-01-31 12:49:00 -0500

    add google analytics

--- web/src/index.js
+++ web/src/index.js
@@ -6,5 +6,13 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './skeleton.css';
 
+// google analytics check
+const GA_KEY = 'REACT_APP_GA_ID';
+if (!process.env[GA_KEY]) {
+  console.error(
+    `You need to specify ${GA_KEY} in your environment, consider putting it in a .env file in the root of the react app`
+  );
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
